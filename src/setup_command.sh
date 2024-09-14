96ebc910c369bf6dcaf9b4cb5cc17ae7344d1d2f
Tuan Hiep <ithadev.nguyen@gmail.com>
2024-09-14 15:18:19 +0700

    fix: fix src path nvim can not apply config

--- src/setup_command.sh
+++
@@ -1,4 +0,0 @@
-echo "# this file is located in 'src/setup_command.sh'"
-echo "# code for 'ithacli setup' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
