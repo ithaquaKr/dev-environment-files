8d881d33dd7b19ca033c63805c484f87c72f5656
Tuan Hiep <ithadev.nguyen@gmail.com>
2024-10-18 18:00:38 +0700

    feat(nvim,wezterm,yabai): update config

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -8,16 +8,16 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.colors = {
-  foreground = "#D6DBE5",
-  --	background = "#131313",
-  background = "#111111",
-  cursor_bg = "#47FF9C",
-  cursor_border = "#47FF9C",
-  cursor_fg = "#011423",
-  selection_bg = "#706b4e",
-  selection_fg = "#f3d9c4",
-  ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-  brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+	foreground = "#D6DBE5",
+	--	background = "#131313",
+	background = "#111111",
+	cursor_bg = "#47FF9C",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
+	selection_bg = "#706b4e",
+	selection_fg = "#f3d9c4",
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 config.font = wezterm.font("FiraCode Nerd Font", { weight = "Regular" })
 config.font_size = 13
