42172a8b6d6927509c6a664a4aa621c772aa9375
Tuan Hiep <ithadev.nguyen@gmail.com>
2025-02-15 21:10:54 +0700

    feat(zsh,wezterm,nvim): upgrade configurations

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require("wezterm")
+local wezterm = require "wezterm"
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -8,25 +8,25 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.colors = {
-	foreground = "#D6DBE5",
-	--	background = "#131313",
-	background = "#111111",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#706b4e",
-	selection_fg = "#f3d9c4",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+  foreground = "#D6DBE5",
+  background = "#000000",
+  -- background = "#111111",
+  cursor_bg = "#47FF9C",
+  cursor_border = "#47FF9C",
+  cursor_fg = "#011423",
+  selection_bg = "#706b4e",
+  selection_fg = "#f3d9c4",
+  ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+  brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 config.font = wezterm.font("FiraCode Nerd Font", { weight = "Regular" })
-config.font_size = 13
+config.font_size = 16
 
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.90
-config.macos_window_background_blur = 35
+config.window_background_opacity = 0.80
+config.macos_window_background_blur = 30
 
 -- and finally, return the configuration to wezterm
 return config
