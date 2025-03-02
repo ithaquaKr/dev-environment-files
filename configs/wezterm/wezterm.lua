8ad19c32c6afeaa2e1aa89e3293c73d5baad81cd
Tuan Hiep <ithadev.nguyen@gmail.com>
2025-03-02 20:55:41 +0700

    feat(wezterm): resize font to see more

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -20,7 +20,7 @@ config.colors = {
   brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 config.font = wezterm.font("FiraCode Nerd Font", { weight = "Medium" })
-config.font_size = 15
+config.font_size = 12
 
 config.enable_tab_bar = false
 
