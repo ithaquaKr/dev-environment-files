1657dab1570999e70c1f9cfeae5095439db24717
Tuan Hiep <ithadev.nguyen@gmail.com>
2025-03-13 18:27:04 +0700

    fix(nvim): fix obsidian.nvim frontmatter configs

--- configs/nvim/lua/plugins/obsidian.lua
+++ configs/nvim/lua/plugins/obsidian.lua
@@ -50,20 +50,19 @@ return {
     ---@return table
     note_frontmatter_func = function(note)
       -- Add the title of the note as an alias.
-      if note.title then
+      if note.title and note.title ~= "Action items" then
         note:add_alias(note.title)
       end
 
-      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+      local out = { id = note.id, aliases = note.aliases }
 
       -- `note.metadata` contains any manually added fields in the frontmatter.
       -- So here we just make sure those fields are kept in the frontmatter.
-      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-        for k, v in pairs(note.metadata) do
-          out[k] = v
-        end
-      end
-
+      -- if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+      --   for k, v in pairs(note.metadata) do
+      --     out[k] = v
+      --   end
+      -- end
       return out
     end,
     -- see below for full list of options 👇
