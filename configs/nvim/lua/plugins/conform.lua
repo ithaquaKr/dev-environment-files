3fb431339d85b9be75c96bde2c583489722c6917
Tuan Hiep <ithadev.nguyen@gmail.com>
2026-01-29 17:38:16 +0700

    feat: update ghostty, tmux, nvim and zsh configs

--- configs/nvim/lua/plugins/conform.lua
+++ configs/nvim/lua/plugins/conform.lua
@@ -3,6 +3,20 @@ return {
   opts = {
     formatters_by_ft = {
       ["python"] = { "ruff_format" },
+      ["php"] = { "php-cs-fixer" },
+      ["astro"] = { "prettier" },
+    },
+    notify_on_error = true,
+    formatters = {
+      ["php-cs-fixer"] = {
+        command = "php-cs-fixer",
+        args = {
+          "fix",
+          "--rules=@PSR12", -- Formatting preset. Other presets are available, see the php-cs-fixer docs.
+          "$FILENAME",
+        },
+        stdin = false,
+      },
     },
   },
 }
