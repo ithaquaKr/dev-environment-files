236f4dee1eab36178c1b1d2ef341ca271618cd16
Tuan Hiep <ithadev.nguyen@gmail.com>
2026-02-07 11:05:45 +0700

    feat: add keymaps for copy buffer path

--- configs/nvim/lua/config/keymaps.lua
+++ configs/nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Copy relative path to clipboard
+vim.keymap.set("n", "<leader>fp", function()
+  local path = vim.fn.expand("%")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied relative path: " .. path)
+end, { desc = "Copy relative file path" })
+
+-- Copy absolute path to clipboard
+vim.keymap.set("n", "<leader>fP", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied absolute path: " .. path)
+end, { desc = "Copy absolute file path" })
