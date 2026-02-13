4357e34141da521c10587772eaf5e37d19dd4307
Tuan Hiep <ithadev.nguyen@gmail.com>
2026-02-13 15:00:26 +0700

    feat: update btop to v1.4.6 config format and switch font to JetbrainsMono Nerd Font

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -34,7 +34,8 @@ config.colors = {
   ansi = { "#15161e", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff", "#a9b1d6" },
   brights = { "#414868", "#ff899d", "#9fe044", "#faba4a", "#8db0ff", "#c7a9ff", "#a4daff", "#c0caf5" },
 }
-config.font = wezterm.font("FiraCode Nerd Font", { weight = "Medium" })
+-- config.font = wezterm.font("FiraCode Nerd Font", { weight = "Medium" })
+config.font = wezterm.font("JetbrainsMono Nerd Font", { weight = "Medium" })
 config.font_size = 12
 
 config.enable_tab_bar = false
