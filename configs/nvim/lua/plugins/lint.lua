e687145ba7d78ee1edb35c1edf2a92d50390d6a3
Tuan Hiep <ithadev.nguyen@gmail.com>
2026-08-05 15:32:34 +0700

    chore(nvim): configure markdownlint args and disable MD013

--- configs/nvim/lua/plugins/lint.lua
+++ configs/nvim/lua/plugins/lint.lua
@@ -1,8 +1,23 @@
+local HOME = os.getenv("HOME")
+
 return {
   "mfussenegger/nvim-lint",
   opts = {
     linters_by_ft = {
       ["php"] = {}, -- disable phpcs completely
     },
+    linters = {
+      markdownlint = {
+        args = { "--disable", "MD013", "--" },
+      },
+      ["markdownlint-cli2"] = {
+        args = { "--config", HOME .. "/.markdownlint-cli2.yaml", "--" },
+      },
+    },
   },
 }
+
+-- The $HOME/.markdownlint-cli2.yaml looks like this:
+--
+-- config:
+--   MD013: false
